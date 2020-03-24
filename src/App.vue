2c246225ffd6ed7e399e2ffbb839e221bf9edf06
Mrigank Anand <mrigankanand@Mriganks-MacBook-Pro.local>
2020-03-24 10:23:27 +0530

    Navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Navbar />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Navbar
+  },
+  methods(){
+
+  }
 }
 </script>
 
---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="navbar">
+      <nav class="nav-extended black darken-2">
+         <div class="nav-content">
+             <router-link to="">
+                 <span class="nav-title">To Do List</span></router-link>
+             <a href="" class="btn-floating btn-large halfway-fab grey">
+                <router-link to="">
+                    <i class="material-icons">add</i>
+                </router-link>
+             </a>
+         </div>
+      </nav>
+    </div>
+</template>
+<script>
+export default {
+    name:'Navbar',
+    data(){
+
+    }
+}
+</script>
+<style scoped>
+.navbar nav {
+    padding:0 40px;
+}
+</style>
